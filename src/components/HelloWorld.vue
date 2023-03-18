68229309fcbf9e4cf7303cff7b4b8ea0aadc7a48
ZhangCanxiang <zhangxiang2010-www@163.com>
2023-03-18 13:56:34 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>hello {{ name }}, this is helloworld components</div>
+</template>
+
+<script setup>
+  const name = ref('HelloWorld')
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+const handleClick = () => {
+  AMessage.info('hello isboyjc, click mini AButton!')
+};
+
+</script>
+<template>
+  <div>hello isboyjc, This is toolsdog home page!</div>
+  <a-space>
+    <a-button type="primary" @click="handleClick">Primary</a-button>
+    <a-button>Secondary</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="outline">Outline</a-button>
+    <a-button type="text">Text</a-button>
+    <span><icon-ep-alarm-clock class=""/> </span>
+  </a-space>
+  <HelloWorld />
+</template>
+<style scoped></style>
